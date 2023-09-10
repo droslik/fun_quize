09008d40912410a5644e7b874faa66d720faf236
Dmitry <roslikd@rocketdata.io>
2023-09-10 16:10:19 +0300

    first commit

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER fun-quize SUPERUSER;
+CREATE DATABASE fun-quize OWNER fun-quize ENCODING 'utf-8';
